bc4bf53aebaa1a553054ea60159dc7dc9649f64a
Justin Phillips <justinphil02@yahoo.com>
2019-11-08 22:37:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer
-  attr_accessor :sender, :receiver, :value
+  attr_accessor :sender, :receiver, :value, :status
   attr_reader :last_transaction
 
   def initialize(sender, receiver, value)
     @sender = sender
     @receiver = receiver
     @value = value
+    @status = pending 
   end
 
 end
