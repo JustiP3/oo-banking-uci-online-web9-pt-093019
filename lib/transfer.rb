cdb2af97575e21eed47f1d6fd5a5a7be2a7ba03e
Justin Phillips <justinphil02@yahoo.com>
2019-11-08 22:32:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+  attr_accessor :account1, :account2, :value
+  attr_reader :last_transaction
+  def initialize(act1, act2, value)
+    @account1 = act1
+    @account2 = act2
+    @value = value
+  end
+
+
 end
